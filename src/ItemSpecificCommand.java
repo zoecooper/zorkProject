1b3b0c0496bc35aab5f07ba123a80aa2bd7b9d1b
Zoe Cooper <zoeccooper@yahoo.com>
2018-10-28 21:16:56 +0000

    Adding itemspecificcommand class -ZCC

---
+++ src/ItemSpecificCommand.java
@@ -0,0 +1,14 @@
+class ItemSpecificCommand extends Command {
+
+    private String verb;
+    private String noun;
+                        
+
+    ItemSpecificCommand(String verb, String noun) {
+        this.verb = verb;
+        this.noun = noun;
+    }
+
+    //execute() should go here
+
+} 
