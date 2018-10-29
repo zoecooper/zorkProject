fe02a4c06c0a33d0b84321770c412a02ddbab6e7
Zoe Cooper <zoeccooper@yahoo.com>
2018-10-29 20:50:59 +0000

    Adding execute() ... -ZCC

--- src/ItemSpecificCommand.java
+++ src/ItemSpecificCommand.java
@@ -10,6 +10,17 @@ class ItemSpecificCommand extends Command {
     }
 
     //execute() should go here
-    public String execute(){
+    public String execute()throws Item.NoItemException {
+        
+        Item itemReferredTo = null;
+        try {
+            itemReferredTo = GameState.instance().getItemInVicinityNamed(noun);
+        } catch (Item.NoItemException e) {
+            return "There's no " + noun + " here.";
+        }
+        
+        String msg = itemReferredTo.getMessageForVerb(verb);
+        return (msg == null ? 
+            "Sorry, you can't " + verb + " the " + noun + "." : msg) + "\n";
     }
 } 
