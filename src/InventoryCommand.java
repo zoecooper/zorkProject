a09857b2a329c0c7dab86f9760d447487ba7f7b8
Aneesa <csnoor08@gmail.com>
2018-10-30 20:12:33 +0000

    Updated my .zork file-AN

--- src/InventoryCommand.java
+++ src/InventoryCommand.java
@@ -6,7 +6,7 @@ class InventoryCommand extends Command {
 	}
 
 	public String execute() {
-		ArrayList<String> names = GameState.instance().getInventoryNamed();
+		ArrayList<String> names = GameState.instance().getInventoryNames();
 
 			if (names.size() == 0) {
 				return "You are empty handed.\n";
