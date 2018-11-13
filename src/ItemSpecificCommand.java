5cc71a958a63254a4918f20c816734fe44ea06bc
Zoe Cooper <zoeccooper@yahoo.com>
2018-11-13 22:47:59 +0000

    Adding the class and method comments for this file -ZCC

--- src/ItemSpecificCommand.java
+++ src/ItemSpecificCommand.java
@@ -1,15 +1,24 @@
-
+/**
+ * Controls tasks that only items can do.
+ * @author ZC
+ */
 class ItemSpecificCommand extends Command {
 
     private String verb;
     private String noun;
                         
-
+    //constructor
     ItemSpecificCommand(String verb, String noun) {
         this.verb = verb;
         this.noun = noun;
     }
 
+   /**
+    *Executes the command for the item.
+    *@return a string saying the desired item does not exist in the room if it does not or a string saying the user cannot do something to the item given if they did not provide the correct command for that specific item
+    *@throws NoItemException if the item given is not the in room the adventurer is currently in
+    *@author ZC
+    */
     public String execute() {
         
         Item itemReferredTo = null;
