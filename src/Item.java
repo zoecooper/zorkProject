ef3445ddb88cdda287216035b9bc29c02f59770b
Zoe Cooper <zoeccooper@yahoo.com>
2018-11-13 23:35:51 +0000

    Adding the class and method comments -ZC

--- src/Item.java
+++ src/Item.java
@@ -4,6 +4,10 @@ import java.util.Hashtable;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * An <tt>Item</tt> represents an item in one of the rooms the player enters and can interact with.
+ * @author ZC
+ */
 public class Item {
 
     static class NoItemException extends Exception {}
@@ -46,11 +50,22 @@ public class Item {
             verbLine = s.nextLine();
         }
     }
-
+    
+    /**
+     * Gets the weight of an item in a room.
+     * @return the item's weight
+     * @author ZC
+     */
     int getWeight() {
         return weight;
     }
 
+   /**
+    * Compares the primary name with alias name.
+    * @param name string of the item
+    * @return a true if the primary name/alias name equals the name or a false if neither of those work.
+    * @author ZC
+    */ 
     boolean goesBy(String name) {
         if (this.primaryName.equals(name)) {
             return true;
@@ -63,12 +78,28 @@ public class Item {
         return false;
     }
 
+   /**
+    * Gets the name of the item.
+    * @return a string that holds the name of the item
+    * @author ZC
+    */
     String getPrimaryName() { return primaryName; }
-
+    
+   /**
+    * Gets the message of the command.
+    * @param verb string that is the command the user wants to use on the item
+    * @return a string of the message
+    * @author ZC
+    */
     public String getMessageForVerb(String verb) {
         return messages.get(verb);
     }
 
+   /**
+    * Gives the name of the item in a string.
+    * @return a string of the item's name
+    * @author ZC
+    */
     public String toString() {
         return primaryName;
     }
