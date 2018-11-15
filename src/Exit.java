b7af3519e330b270651d022c4c3480da422b7c37
Aneesa <csnoor08@gmail.com>
2018-11-15 04:23:42 +0000

    Edited -AN

--- src/Exit.java
+++ src/Exit.java
@@ -1,13 +1,22 @@
 
 import java.util.Scanner;
-
+/**
+ * @author AN
+ */ 
 public class Exit {
 
     class NoExitException extends Exception {}
 
     private String dir;
     private Room src, dest;
-
+    /**
+     * Takes a String and two Rooms objects to construct an Exit object.
+     * After initializing the source Room object's addExit method is called 
+     * to add the exit into the Room's exit. 
+     *@param dir direction of the exit with the source room
+     *@param src room object containing the exit
+     *@param dest destination room object
+     */ 
     Exit(String dir, Room src, Room dest) {
         init();
         this.dir = dir;
@@ -53,12 +62,28 @@ public class Exit {
     // Common object initialization tasks.
     private void init() {
     }
-
+    /**
+     * This method is used to return a message describing the exit 
+     * object's dir variable and the name of the variable of the destination 
+     * Room object.
+     * @return message describing the exits direction and destination
+     */  	
     String describe() {
         return "You can go " + dir + " to " + dest.getTitle() + ".";
     }
-
+    /**
+     * Returns the direction variable
+     * @return exit dir
+     */ 	
     String getDir() { return dir; }
+    /**
+     * Returns the Room which the exit is in. 
+     * @return exit source room
+     */ 
     Room getSrc() { return src; }
+    /**
+     * Returns the destination Room of the exit.
+     * @return destination of exit
+     */ 
     Room getDest() { return dest; }
 }
