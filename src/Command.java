7e428e93771d9dc1874959e2a5e007b6687868f6
Zoe Cooper <zoeccooper@yahoo.com>
2018-11-20 03:05:55 +0000

    Fixing -ZC

--- src/Command.java
+++ src/Command.java
@@ -1,11 +1,12 @@
 /**
- * Is the parent class of all the commands sub-classes
- * which interacts with users input and is parsed by 
+ * The parent class of all the commands sub-classes
+ * which interacts with user's input and is parsed by 
  * the CommandFactory class. 
  * @author AN
  */
 abstract class Command {
     /**
+     * Executes the command.
      * @return a string describing where you went and what you did
      */	
     abstract String execute();
