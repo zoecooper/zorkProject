dcd628fe43237a31e62b25a7c6413ef1855e979f
Zoe Cooper <zoeccooper@yahoo.com>
2018-11-30 00:43:02 +0000

    Fixing string -ZC

--- src/Event.java
+++ src/Event.java
@@ -1,5 +1,15 @@
-abstract class Event{
+/**
+ * The parent class of all the commands sub-classes
+ * which interacts with user's input and is parsed by 
+ * the CommandFactory class. 
+ * @author ZC
+ */
+abstract class Event {
+        /**
+	 * Executes the event command.
+	 * @return a string with the event occurrence
+	 */
 
-	abstract void execute();
+	abstract String execute();
 
 }
