67f50e02a534be93796ab08eff665fe26169d8d7
Zoe Cooper <zoeccooper@yahoo.com>
2018-12-02 22:38:32 +0000

    O -ZC

--- src/TransformEvent.java
+++ src/TransformEvent.java
@@ -13,7 +13,7 @@ class TransformEvent extends Event {
                 //disappears it
                 GameState.instance().getDungeon().disappearItem(String.valueOf(theItem));
                 //new item
-                Item newItem = GameState.instance().getDungeon().getItem(n.substring(n.indexOf("(")+1, n.indexOf(")")));
+                Item newItem = GameState.instance().getDungeon().getItem(o.substring(o.indexOf("(")+1, o.indexOf(")")));
 
 
                 GameState.instance().addToInventory(newItem);
